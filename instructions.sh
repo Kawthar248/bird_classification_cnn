b8290f6d1ab2f81635ff3612761fae872162c915
kawthar248 <kayy.2408@hotmail.com>
2024-04-13 17:49:26 +0300

    update

--- instructions.sh
+++ instructions.sh
@@ -6,8 +6,6 @@ docker run -v $(pwd)/data:/data -e KAGGLE_USERNAME=$KAGGLE_USERNAME -e KAGGLE_KE
 git checkout -b train
 cp -r ../data ./data
 
-## Train
-sudo unzip ./data/100-bird-species.zip -d ./datadata/
-
+## Trainp
 docker build -t bird-classification .
 docker run bird-classification
